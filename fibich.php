e0a9a59bf3639309c0d198329b9e538c7ac6dfd1
Paolo Gallardo <paolo.j.gallardo@gmail.com>
2020-11-15 15:40:50 +0800

    plugin version update

--- fibich.php
+++ fibich.php
@@ -5,7 +5,7 @@
    * Description: Custom plugin to add custom functionalities based on client requests
    * Author: Paolo Gallardo | Waymaker Designs
    * Author URI: https:github.com/payatola2287
-   * Version: 1.2
+   * Version: 2.0
    */
 
 use BisonPlugin\BisonPlugin;
