f8c980870ed3cdf8950363b2038f5949ceae66e4
Paolo Gallardo <paolo.j.gallardo@gmail.com>
2020-11-15 16:15:50 +0800

    plugin update check

--- fibich.php
+++ fibich.php
@@ -5,13 +5,18 @@
    * Description: Custom plugin to add custom functionalities based on client requests
    * Author: Paolo Gallardo | Waymaker Designs
    * Author URI: https:github.com/payatola2287
-   * Version: 2.1
+   * Version: 2.2
    */
 
 use BisonPlugin\BisonPlugin;
 
 require('class-bison.php');
-
+include('plugin-update-checker/plugin-update-checker.php');
 $instance = new BisonPlugin();
 
-$instance->do_update_checks(__FILE__);
+$pluginUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  'https://github.com/payatola2287/fibichlawfirmcustomcode/',
+  $dir,
+  'fibichlawfirmcustomcode'
+);
+$pluginUpdateChecker->setBranch('production');
